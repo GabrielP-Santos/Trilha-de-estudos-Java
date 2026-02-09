28bf3c4e1cecd5774f39f0077a22395f772abcfe
Gabriel dos Santos <GabrielPSantos.bel@gmail.com>
2026-02-09 10:27:08 -0300

    commit criação de uma exceção

---
+++ src/Exercicios/poo/Unidade2cap5/ex08/IdadeInvalidaException.java
@@ -0,0 +1,7 @@
+package Exercicios.poo.Unidade2cap5.ex08;
+
+public class IdadeInvalidaException extends RuntimeException {
+    public IdadeInvalidaException(String message) {
+        super(message);
+    }
+}
